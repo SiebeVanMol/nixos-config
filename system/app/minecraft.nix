994d0fdfd4e9dcac8353b582e62c46397cf053bf
Snowy Renard <snowyrenard@gmail.com>
2026-07-19 16:09:13 +0200

    minecraft server working

--- system/app/minecraft.nix
+++ system/app/minecraft.nix
@@ -1,3 +1,17 @@
+{ pkgs, lib, ... }:
+let
+  mcVersion = "1.21.1";
+  forgeVersion = "21.1.234";
+  mcVersionUnderscored = lib.replaceStrings [ "." ] [ "_" ] mcVersion;
+  forgeVersionUnderscored = lib.replaceStrings [ "." ] [ "_" ] forgeVersion;
+  serverVersion = "neoforge-${mcVersionUnderscored}-${forgeVersionUnderscored}";
+
+  atmonsServerPack = pkgs.fetchzip {
+    url = "https://mediafilez.forgecdn.net/files/8431/25/ServerFiles-1.1.1.zip";
+    stripRoot = false;
+    sha256 = "sha256-KpOoctVm2tTNKu/dUNHTfj+Xyh/1iC5fNnRA7t/3K1o=";
+  };
+in
 {
   services.minecraft-servers = {
     enable = true;
@@ -7,8 +21,32 @@
       enable = true;
       openFirewall = true;
 
-      jvmOpts = "-Xms4096M -Xmx8192M";
+      package = pkgs.neoforgeServers.${serverVersion}.override {
+        jre_headless = pkgs.jdk21_headless;
+      };
+
+      serverProperties = {
+        difficulty = 3;
+        gamemode = "survival";
+        motd = "violet town";
+        allow-cheats = true;
+        allow-flight = true;
+        max-tick-time = 180000;
+        simulation-distance = 5;
+        view-distance = 8;
+      };
+
+      symlinks = {
+        "datapacks" = "${atmonsServerPack}/datapacks";
+        "user_jvm_args.txt" = "${atmonsServerPack}/user_jvm_args.txt";
+        "server-icon.png" = "${atmonsServerPack}/server-icon.png";
+      };
+      files = {
+        "mods" = "${atmonsServerPack}/mods";
+        "config" = "${atmonsServerPack}/config";
+        "kubejs" = "${atmonsServerPack}/kubejs";
+      };
+      jvmOpts = "-Xms4G -Xmx16G";
     };
   };
 }
-
