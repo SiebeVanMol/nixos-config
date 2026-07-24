640387ebf445209b5614507ec4c6b9639632d8a3
Snowy Renard <snowyrenard@gmail.com>
2026-07-24 11:42:57 +0200

    fix: use string concat in swww_randomize to avoid Nushell $ interpol parsing bug

--- home/wayland.nix
+++ home/wayland.nix
@@ -44,7 +44,10 @@
             try {
               let c10 = (open ~/.cache/wallust/hypr-colors | lines | where ($it =~ "color10") | first | parse "color10 = \"{c}\"" | get c.0)
               let c12 = (open ~/.cache/wallust/hypr-colors | lines | where ($it =~ "color12") | first | parse "color12 = \"{c}\"" | get c.0)
-              hyprctl eval $"hl.config({ general = { col = { active_border = { colors = {\"rgb($c10)\", \"rgb($c12)\"} }, inactive_border = \"rgba(ffffffbb)\" } } })" | ignore
+              let v1 = ("\"" + "rgb(" + $c10 + ")" + "\"")
+              let v2 = ("\"" + "rgb(" + $c12 + ")" + "\"")
+              let cmd = ("hl.config({ general = { col = { active_border = { colors = {" + $v1 + ", " + $v2 + "} }, inactive_border = \"rgba(ffffffbb)\" } } })")
+              hyprctl eval $cmd | ignore
             }
             pkill waybar -SIGUSR2 | ignore
           }
