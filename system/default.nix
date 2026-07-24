a9b5dab16c56da94176f06917ca321cbbe8530ea
Snowy Renard <snowyrenard@gmail.com>
2026-07-24 12:42:55 +0200

    Revert "feat: add tmpfs for /tmp with 4G limit"
    This reverts commit 0f7e107f25746b2affaf51586f8d24a6fbd542a5.

--- system/default.nix
+++ system/default.nix
@@ -27,10 +27,6 @@
   # Periodic SSD TRIM.
   services.fstrim.enable = true;
 
-  # Put /tmp on tmpfs with a 4G cap instead of the default 50% of RAM.
-  boot.tmp.useTmpfs = true;
-  boot.tmp.tmpfsSize = "4G";
-
   # Allow unfree packages.
   nixpkgs.config.allowUnfree = true;
 
