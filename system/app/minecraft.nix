670fd4e4e8c2275143d7ae2bda69b17a7be025f1
Snowy Renard <snowyrenard@gmail.com>
2026-07-25 15:48:10 +0200

    refactor: extract extra mods into let binding

--- system/app/minecraft.nix
+++ system/app/minecraft.nix
@@ -55,6 +55,53 @@ let
     outputHashAlgo = "sha256";
     outputHash = "sha256-r9urTFPVcxkgzwFJdffzva35H5DjqvHKY4beoJgLOOs=";
   };
+
+  extraMods = {
+    "tectonic-3.0.26-fabric-21.1.jar" = pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/lWDHr9jE/versions/L87Phsbl/tectonic-3.0.26-fabric-21.1.jar";
+      hash = "sha256-ZlO+fLFXTllzzeCoysudednlWpm/c3iTfgxHl5txi2c=";
+    };
+    "Terralith_1.21.x_v2.6.2.jar" = pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/8oi3bsk5/versions/eWDLFabb/Terralith_1.21.x_v2.6.2.jar";
+      hash = "sha256-nNTUAv3g9SPltDCsj9R5zgWup6UP4MjCaQH192knIhQ=";
+    };
+    "regions-unexplored-0.6.2-fabric-21.1.jar" = pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/Tkikq67H/versions/SffwLsGY/regions-unexplored-0.6.2-fabric-21.1.jar";
+      hash = "sha256-wMLxqDy9wfJSJVgj4zIwrOnbb1/W2gEKc59G96bTpqc=";
+    };
+    "YungsBetterDungeons-1.21.1-Fabric-5.1.4.jar" = pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/o1C1Dkj5/versions/fQ7EjDPE/YungsBetterDungeons-1.21.1-Fabric-5.1.4.jar";
+      hash = "sha256-af59k6+hgD12WN8/hhIT+CVfaNTpdtZt3OZvugjRILw=";
+    };
+    "YungsBetterMineshafts-1.21.1-Fabric-5.1.1.jar" = pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/HjmxVlSr/versions/4ybDuGhA/YungsBetterMineshafts-1.21.1-Fabric-5.1.1.jar";
+      hash = "sha256-J5SfW64K/v9FdxGltCSCvjAbdryLETbxEg+EhIm7X6Y=";
+    };
+    "YungsBetterStrongholds-1.21.1-Fabric-5.1.3.jar" = pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/kidLKymU/versions/uYZShp1p/YungsBetterStrongholds-1.21.1-Fabric-5.1.3.jar";
+      hash = "sha256-HLQSyDqg6Cc9KaESLFeZ6xWkisyPC3cO/kUcUWZH9fg=";
+    };
+    "YungsBetterOceanMonuments-1.21.1-Fabric-4.1.2.jar" = pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/3dT9sgt4/versions/TGK6gpeO/YungsBetterOceanMonuments-1.21.1-Fabric-4.1.2.jar";
+      hash = "sha256-TPuyJr0dsXAyrH5zBulO0n8aji8+CftixHaI+KUnrew=";
+    };
+    "YungsBetterWitchHuts-1.21.1-Fabric-4.1.1.jar" = pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/t5FRdP87/versions/bdpPtvTn/YungsBetterWitchHuts-1.21.1-Fabric-4.1.1.jar";
+      hash = "sha256-lU/wBN4VFlLZvmngPRuEjW/RWg9fLfFcTOLnZEZ2hec=";
+    };
+    "YungsApi-1.21.1-Fabric-5.1.6.jar" = pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/Ua7DFN59/versions/9aZPNrZC/YungsApi-1.21.1-Fabric-5.1.6.jar";
+      hash = "sha256-NvuQOh688VEXRb4tqeUUTx0kZb/3pcF77/00c5ms0bo=";
+    };
+    "TerraBlender-fabric-1.21.1-4.1.0.8.jar" = pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/kkmrDlKT/versions/XNtIBXyQ/TerraBlender-fabric-1.21.1-4.1.0.8.jar";
+      hash = "sha256-+H6Up/oSJ3EcP4rqn/rHoU4Me+IS/lDd7pXSxrpyPKw=";
+    };
+    "ctov-3.6.3.jar" = pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/fgmhI8kH/versions/dqaObRbU/%5BFabric%5Dctov-3.6.3.jar";
+      hash = "sha256-5EOSXY/k0JLx85Ji+nMYLknjTcv5ylHqqlMGS7ku5lI=";
+    };
+  };
 in
 {
   config = lib.mkIf config.device.app.minecraft.enable {
@@ -82,52 +129,7 @@ in
         files = {
           "mods" = "${cobbleverse}/mods";
           "config" = "${cobbleverse}/config";
-
-          "mods/tectonic-3.0.26-fabric-21.1.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/lWDHr9jE/versions/L87Phsbl/tectonic-3.0.26-fabric-21.1.jar";
-            hash = "sha256-ZlO+fLFXTllzzeCoysudednlWpm/c3iTfgxHl5txi2c=";
-          };
-          "mods/Terralith_1.21.x_v2.6.2.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/8oi3bsk5/versions/eWDLFabb/Terralith_1.21.x_v2.6.2.jar";
-            hash = "sha256-nNTUAv3g9SPltDCsj9R5zgWup6UP4MjCaQH192knIhQ=";
-          };
-          "mods/regions-unexplored-0.6.2-fabric-21.1.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/Tkikq67H/versions/SffwLsGY/regions-unexplored-0.6.2-fabric-21.1.jar";
-            hash = "sha256-wMLxqDy9wfJSJVgj4zIwrOnbb1/W2gEKc59G96bTpqc=";
-          };
-          "mods/YungsBetterDungeons-1.21.1-Fabric-5.1.4.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/o1C1Dkj5/versions/fQ7EjDPE/YungsBetterDungeons-1.21.1-Fabric-5.1.4.jar";
-            hash = "sha256-af59k6+hgD12WN8/hhIT+CVfaNTpdtZt3OZvugjRILw=";
-          };
-          "mods/YungsBetterMineshafts-1.21.1-Fabric-5.1.1.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/HjmxVlSr/versions/4ybDuGhA/YungsBetterMineshafts-1.21.1-Fabric-5.1.1.jar";
-            hash = "sha256-J5SfW64K/v9FdxGltCSCvjAbdryLETbxEg+EhIm7X6Y=";
-          };
-          "mods/YungsBetterStrongholds-1.21.1-Fabric-5.1.3.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/kidLKymU/versions/uYZShp1p/YungsBetterStrongholds-1.21.1-Fabric-5.1.3.jar";
-            hash = "sha256-HLQSyDqg6Cc9KaESLFeZ6xWkisyPC3cO/kUcUWZH9fg=";
-          };
-          "mods/YungsBetterOceanMonuments-1.21.1-Fabric-4.1.2.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/3dT9sgt4/versions/TGK6gpeO/YungsBetterOceanMonuments-1.21.1-Fabric-4.1.2.jar";
-            hash = "sha256-TPuyJr0dsXAyrH5zBulO0n8aji8+CftixHaI+KUnrew=";
-          };
-          "mods/YungsBetterWitchHuts-1.21.1-Fabric-4.1.1.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/t5FRdP87/versions/bdpPtvTn/YungsBetterWitchHuts-1.21.1-Fabric-4.1.1.jar";
-            hash = "sha256-lU/wBN4VFlLZvmngPRuEjW/RWg9fLfFcTOLnZEZ2hec=";
-          };
-          "mods/YungsApi-1.21.1-Fabric-5.1.6.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/Ua7DFN59/versions/9aZPNrZC/YungsApi-1.21.1-Fabric-5.1.6.jar";
-            hash = "sha256-NvuQOh688VEXRb4tqeUUTx0kZb/3pcF77/00c5ms0bo=";
-          };
-          "mods/TerraBlender-fabric-1.21.1-4.1.0.8.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/kkmrDlKT/versions/XNtIBXyQ/TerraBlender-fabric-1.21.1-4.1.0.8.jar";
-            hash = "sha256-+H6Up/oSJ3EcP4rqn/rHoU4Me+IS/lDd7pXSxrpyPKw=";
-          };
-          "mods/ctov-3.6.3.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/fgmhI8kH/versions/dqaObRbU/%5BFabric%5Dctov-3.6.3.jar";
-            hash = "sha256-5EOSXY/k0JLx85Ji+nMYLknjTcv5ylHqqlMGS7ku5lI=";
-          };
-        };
+        } // lib.mapAttrs' (name: drv: lib.nameValuePair "mods/${name}" drv) extraMods;
 
         jvmOpts = "-Xms8G -Xmx32G -Dfml.readTimeout=120 -Dfml.connectionTimeout=120";
       };
