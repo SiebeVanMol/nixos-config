1522e629a52bca6e5ef13126a2f0fb31cdd0ba32
Snowy Renard <snowyrenard@gmail.com>
2026-08-14 19:19:45 +0200

    enable hyprland xwayland

--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -6,6 +6,7 @@
   config = lib.mkIf config.device.wm.hyprland.enable {
     programs.hyprland.enable = true;
     programs.hyprland.withUWSM = true;
+    programs.hyprland.xwayland.enable = true;
     services.displayManager.generic.environment.XDG_CURRENT_DESKTOP = "X-NIXOS-SYSTEMD-AWARE";
   };
 }
